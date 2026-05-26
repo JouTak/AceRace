3a76405cd58336d705b472c008a1c5c8d8c34479
Denis K. Berman (EnderDissa) <enderdissa@gmail.com>
2026-05-26 21:07:56 +0300

    hotfix: idk old fix

--- src/main/kotlin/com/joutak/acerace/listeners/PlayerMoveListener.kt
+++ src/main/kotlin/com/joutak/acerace/listeners/PlayerMoveListener.kt
@@ -6,12 +6,15 @@ import com.joutak.acerace.config.Config
 import com.joutak.acerace.config.ConfigKeys
 import com.joutak.acerace.players.PlayerData
 import com.joutak.acerace.zones.ZoneManager
+import com.joutak.acerace.zones.ZoneType
 import org.bukkit.Bukkit
 import org.bukkit.GameMode
 import org.bukkit.Location
+import org.bukkit.Material
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerMoveEvent
+
 
 class PlayerMoveListener : Listener {
     @EventHandler
@@ -34,8 +37,13 @@ class PlayerMoveListener : Listener {
         }
 
         if (worldName.startsWith("aceracemap") || worldName.startsWith("lobby")) {
+            var insideElytraZone = false
+
             for (zone in ZoneManager.getZones().values) {
                 if (zone.isInside(location)) {
+                    if (zone.type == ZoneType.ELYTRA) {
+                        insideElytraZone = true
+                    }
                     AceRacePlugin.instance.logger.info("Игрок ${player.name} вошел в зону ${zone.name} (${zone.type})")
                     zone.execute(player)
                 }
@@ -49,16 +57,25 @@ class PlayerMoveListener : Listener {
 
             if (player.y <= Config.get(ConfigKeys.Y_DEATH)) {
                 val playerData = PlayerData.get(player.uniqueId)
-                val checkpoint = CheckpointManager.get(playerData.getLastCheck())
-                val loc = Location(
-                    location.world,
-                    (checkpoint.x1 + checkpoint.x2) / 2,
-                    (checkpoint.y1 + checkpoint.y2) / 2,
-                    (checkpoint.z1 + checkpoint.z2) / 2,
-                    checkpoint.yaw,
-                    checkpoint.pitch,
-                )
+                val x1 = CheckpointManager.get(playerData.getLastCheck()).x1
+                val x2 = CheckpointManager.get(playerData.getLastCheck()).x2
+                val y1 = CheckpointManager.get(playerData.getLastCheck()).y1
+                val y2 = CheckpointManager.get(playerData.getLastCheck()).y2
+                val z1 = CheckpointManager.get(playerData.getLastCheck()).z1
+                val z2 = CheckpointManager.get(playerData.getLastCheck()).z2
+                val yaw = CheckpointManager.get(playerData.getLastCheck()).yaw
+                val pitch = CheckpointManager.get(playerData.getLastCheck()).pitch
+
+                val loc = Location(location.world, (x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2, yaw, pitch)
                 player.teleport(loc)
+            }
+
+            if (!insideElytraZone &&
+                player.isOnGround &&
+                !player.isGliding &&
+                player.inventory.chestplate?.type == Material.ELYTRA
+            ) {
+                player.inventory.chestplate = null
             }
         }
     }
